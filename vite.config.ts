2eedd619ea427f09618651206248542bc01040aa
Viktor <viktor@MacBook-Air-Viktor.local>
2025-03-21 13:50:21 +0300

    T

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.mp4']
+  assetsInclude: ['**/*.mp4'],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
